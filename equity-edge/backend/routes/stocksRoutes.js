d181e24e7ea57599d8ac826af6f693cc6e775a77
Cristina <cris.dege0813@gmail.com>
2024-03-10 18:04:28 -0400

    backend wip

--- equity-edge/backend/routes/stocksRoutes.js
+++ equity-edge/backend/routes/stocksRoutes.js
@@ -1,54 +1,62 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Transaction = require('./models/Transaction');
-const User = require('./models/User');
+// const Transaction = require('./models/Transaction');
+// const User = require("./models/User");
 
 const authenticateUser = (req, res, next) => {
   // jwt authentication will go here
-  const userId = req.headers['user-id'];
+  const userId = req.headers["user-id"];
   if (!userId) {
-    return res.status(401).json({ error: 'Unauthorized' });
+    return res.status(401).json({ error: "Unauthorized" });
   }
   req.userId = userId;
   next();
 };
 
-router.get('/user/:userId/transactions', authenticateUser, async (req, res) => {
+router.get("/user/:userId/transactions", authenticateUser, async (req, res) => {
   try {
     const userId = req.params.userId;
     const transactions = await Transaction.find({ userId });
     res.json(transactions);
   } catch (error) {
-    console.error('Error fetching transactions:', error);
-    res.status(500).json({ error: 'Internal server error' });
+    console.error("Error fetching transactions:", error);
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
-router.post('/user/:userId/transactions', authenticateUser, async (req, res) => {
-  try {
-    const userId = req.params.userId;
-    const newTransaction = new Transaction({ userId, ...req.body });
-    await newTransaction.save();
+router.post(
+  "/user/:userId/transactions",
+  authenticateUser,
+  async (req, res) => {
+    try {
+      const userId = req.params.userId;
+      const newTransaction = new Transaction({ userId, ...req.body });
+      await newTransaction.save();
 
-    res.status(201).json(newTransaction);
-  } catch (error) {
-    console.error('Error adding transaction:', error);
-    res.status(500).json({ error: 'Internal server error' });
+      res.status(201).json(newTransaction);
+    } catch (error) {
+      console.error("Error adding transaction:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
   }
-});
+);
 
-router.delete('/user/:userId/transactions/:transactionId', authenticateUser, async (req, res) => {
-  try {
-    const userId = req.params.userId;
-    const transactionId = req.params.transactionId;
+router.delete(
+  "/user/:userId/transactions/:transactionId",
+  authenticateUser,
+  async (req, res) => {
+    try {
+      const userId = req.params.userId;
+      const transactionId = req.params.transactionId;
 
-    await Transaction.deleteOne({ _id: transactionId, userId });
+      await Transaction.deleteOne({ _id: transactionId, userId });
 
-    res.json({ message: 'Transaction canceled successfully' });
-  } catch (error) {
-    console.error('Error canceling transaction:', error);
-    res.status(500).json({ error: 'Internal server error' });
+      res.json({ message: "Transaction canceled successfully" });
+    } catch (error) {
+      console.error("Error canceling transaction:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
   }
-});
+);
 
 module.exports = router;
--- equity-edge/backend/user.js
+++ equity-edge/backend/user.js
@@ -1,18 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('./models/user'); 
-const bcrypt = require('bcrypt');
+// const User = require('./models/user');
+const bcrypt = require("bcrypt");
 
 // new user register
-router.post('/register', async (req, res) => {
+router.post("/register", async (req, res) => {
   try {
     const { username, password } = req.body;
 
     const existingUser = await User.findOne({ username });
     if (existingUser) {
-      return res.status(400).json({ message: 'Username already exists' });
+      return res.status(400).json({ message: "Username already exists" });
     }
-   
+
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const newUser = new User({ username, password: hashedPassword });
@@ -23,17 +23,17 @@ router.post('/register', async (req, res) => {
   }
 });
 
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   try {
     const { username, password } = req.body;
     const user = await User.findOne({ username });
     if (!user) {
-      return res.status(401).json({ message: 'Invalid credentials' });
+      return res.status(401).json({ message: "Invalid credentials" });
     }
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
-      return res.status(401).json({ message: 'Invalid credentials' });
+      return res.status(401).json({ message: "Invalid credentials" });
     }
 
     res.json({ user });
